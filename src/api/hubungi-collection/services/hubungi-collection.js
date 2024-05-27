e435d55742f1ba192d5ded2c015ec75310e4c71c
Dilshan Jayasinghe <dilshanj420@gmail.com>
2024-05-27 22:51:52 +0800

    Update hubungi-collection.js

--- src/api/hubungi-collection/services/hubungi-collection.js
+++ src/api/hubungi-collection/services/hubungi-collection.js
@@ -10,13 +10,14 @@ module.exports = () => ({
         const collections = ['lokasi-johor', 'lokasi-kedah', 'lokasi-kelantan', 'lokasi-melaka', 'lokasi-negeri-sembilan', 'lokasi-pahang', 'lokasi-perak', 'lokasi-perlis', 'lokasi-pulau-pinang', 'lokasi-sabah', 'lokasi-sarawak', 'lokasi-selangor', 'lokasi-terengganu', 'lokasi-wp-kuala-lumpur', 'lokasi-wp-labuan'];  
     
         collections.forEach(collection => {
+
           queryPromises.push(
             strapi.db.query(`api::${collection}.${collection}`).findMany({
               where: {
                 title: {
                   $containsi: keyword  
                 },
-                locationAddress: {
+                location_address: {
                   $containsi: locationKeyword
                 }
               },
