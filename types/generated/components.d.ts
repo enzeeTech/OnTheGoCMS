e95b9ecd96cb8f3875400f2fca09798464095be4
Dilshan Jayasinghe <dilshanj420@gmail.com>
2024-06-12 17:36:55 +0800

    Added new components

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -43,6 +43,46 @@ export interface SubsectionsSection extends Schema.Component {
   };
 }
 
+export interface TicketsDoubleTicket extends Schema.Component {
+  collectionName: 'components_tickets_double_tickets';
+  info: {
+    displayName: 'DoubleTicket';
+  };
+  attributes: {
+    TicketData: Attribute.JSON;
+  };
+}
+
+export interface TicketsSingleTicket extends Schema.Component {
+  collectionName: 'components_tickets_single_tickets';
+  info: {
+    displayName: 'Single Ticket';
+  };
+  attributes: {
+    TicketData: Attribute.JSON;
+  };
+}
+
+export interface TilesImageDescTile extends Schema.Component {
+  collectionName: 'components_tiles_image_desc_tiles';
+  info: {
+    displayName: 'ImageDescTile';
+  };
+  attributes: {
+    TileData: Attribute.JSON;
+  };
+}
+
+export interface TilesPriceTileNoSubtitle extends Schema.Component {
+  collectionName: 'components_tiles_price_tile_no_subtitles';
+  info: {
+    displayName: 'PriceTileNoSubtitle';
+  };
+  attributes: {
+    TileData: Attribute.JSON;
+  };
+}
+
 export interface TilesPriceTile1 extends Schema.Component {
   collectionName: 'components_tiles_price_tile1s';
   info: {
@@ -60,6 +100,10 @@ declare module '@strapi/types' {
       'lists.bullet-point-list': ListsBulletPointList;
       'lists.green-tick-list-box': ListsGreenTickListBox;
       'subsections.section': SubsectionsSection;
+      'tickets.double-ticket': TicketsDoubleTicket;
+      'tickets.single-ticket': TicketsSingleTicket;
+      'tiles.image-desc-tile': TilesImageDescTile;
+      'tiles.price-tile-no-subtitle': TilesPriceTileNoSubtitle;
       'tiles.price-tile1': TilesPriceTile1;
     }
   }
